52ee0d651a7392b73d6bb8ef8e763f47bfad7f9c
Stutiguias <dracondf@gmail.com>
2012-09-25 21:52:08 -0300

    First Setup C# Interface

---
+++ C#/WebPortal/WebPortal/App_Start/BundleConfig.cs
@@ -0,0 +1,23 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace WebPortal
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/js/jquery.unobtrusive*",
+                        "~/js/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/js/modernizr-*"));
+
+            bundles.Add(new StyleBundle("~/css").Include("~/css/main.css"));
+
+            bundles.Add(new StyleBundle("~/css/dark-hive/").Include("~/css/dark-hive/jquery-ui-1.8.18.custom.css"));
+        }
+    }
+}
