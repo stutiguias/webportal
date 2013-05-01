6160b331ea4fc4d2a6352674583c2ddaf0fed12a
dfdgsdfg <dfdgsdfg@gmail.com>
2013-05-01 12:50:15 +0900

    add class in htmls for event binding

--- src/main/java/me/stutiguias/webportal/webserver/Html.java
+++ src/main/java/me/stutiguias/webportal/webserver/Html.java
@@ -20,7 +20,7 @@ public class Html {
     
     public String HTMLBuy(String ip,int ID){
       if(WebPortal.AuthPlayers.get(ip).AuctionPlayer.getCanBuy() == 1) {
-        return "<form action='buy/item' method='GET' onsubmit='return buy(this)'>"+
+        return "<form class='js-buyItems' action='buy/item' method='GET' onsubmit='return buy(this)'>"+
                 "<input type='text' name='Quantity' onKeyPress='return numbersonly(this, event);' class='input' />"+
                 "<input type='hidden' name='ID' value='"+ID+"' />"+
                 "<input type='submit' value='" + plugin.Messages.get("Buy") + "' class='button' /></form><span id='"+ID+"'></span>";
@@ -30,14 +30,14 @@ public class Html {
     }
     
     public String HTMLCancel(String ip,int ID){
-        return "<form action='cancel/auction' method='GET' onsubmit='return cancel(this)'>"+
+        return "<form class='js-cancelAuction' action='cancel/auction' method='GET' onsubmit='return cancel(this)'>"+
                 "<input type='hidden' name='ID' value='"+ID+"' />"+
                 "<input type='submit' value='" + plugin.Messages.get("Cancel") + "' class='button' /></form><span id='C"+ID+"'></span>";
     }
     
     public String HTMLAuctionCreate(String ip,int ID) {
       if(WebPortal.AuthPlayers.get(ip).AuctionPlayer.getCanSell() == 1) {
-        return "<form action='web/postauction' method='GET' onsubmit='return postauction(this)'>"+
+        return "<form class='js-postAuction' action='web/postauction' method='GET' onsubmit='return postauction(this)'>"+
                 "Quantity: <input type='text' name='Quantity' onKeyPress='return numbersonly(this, event);' class='input' /><br />"+
                 "<input type='hidden' name='ID' value='"+ID+"' />"+
                 "Price: <input name='Price' type='text' onKeyPress='return numbersonly(this, event);' class='input' size='10' /><br />"+
@@ -48,7 +48,7 @@ public class Html {
     }
     
     public String HTMLAuctionMail(String ip,int ID) {
-        return "<form action='web/mail' method='GET' onsubmit='return mail(this)'>"+
+        return "<form class='js-mailItems' action='web/mail' method='GET' onsubmit='return mail(this)'>"+
                 "<input type='hidden' name='ID' value='"+ID+"' />"+
                 "<input type='text' name='Quantity' onKeyPress='return numbersonly(this, event);' class='input' size='5' /><br />"+
                 "<input type='submit' value='" + plugin.Messages.get("Mailit") + "' class='button' /></form><span id='M"+ID+"'></span>";
