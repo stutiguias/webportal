aab0590781dccf28219a1e32243c50893dbf0b60
Daniel <dracondf@gmail.com>
2024-03-19 21:15:44 -0300

    update admin

--- html/js/page/admin.js
+++ html/js/page/admin.js
@@ -1,256 +1,205 @@
-var get = function (form, url) {
-    $(".table").show();
-    $(".pagination").html("");
-    hideall();
-    $.ajax({
-        url: window.qualifyURL(url),
-        success: function (data) {
+const vue = new Vue({
+    el: '#app',
+    vuetify: new Vuetify(),
+    data: () => ({
+        to: 0,
+        from: 10,
+        showInfoPlayer: false,
+        showAddItemShop: false,
+        showListAdmShop: false,
+        nick: '',
+        information: '',
+        infoOptions: [
+            { text: 'Player Info', value: 'playerinfo' },
+            { text: 'Player Items', value: 'playeritems' },
+            // Complete with other options
+        ],
+        itemId: '',
+        quantity: '',
+        price: '',
+        resultado: '',
+        tableHeaders: [
+            { text: 'Delete', value: 'delete', sortable: false },
+            { text: 'Item Name', value: 'item_name' },
+            { text: 'Price', value: 'price' },
+            { text: 'Quantity', value: 'quantity' }
+        ],
+        tableItems: [],
+        userInfo: []
+    }),
+    methods: {
+        async get(url) {
             try {
-                var result = "";
-                $(".theadInput").html("");
-                $.each(data, function (key, val) {
-                    $.each(val, function (key, val) {
-                        $(".theadInput").html($(".theadInput").html() + "<th>" + key + "</th>");
-                    });
-                    return false;
-                });
-
-                $.each(data, function (key, val) {
-                    result += "<tr>";
-                    $.each(val, function (key, val) {
-                        result += "<td>" + val + "</td>";
-                    });
-                    result += "</tr>";
-                });
-
-                $(".tbodyInput").html(result);
-            } catch (err) {
-                $('#resultado').html(err.message);
-            };
+                const response = await fetch(window.qualifyURL(url));
+                const data = await response.json();
+                this.processData(data);
+            } catch (error) {
+                this.resultado = error;
+            }
         },
-        error: function (error,data) {
-            $('#resultado').html(error);
+        processData(data) {
+            this.loadTable(data)
         },
-        dataType: "json"
-    });
+        loadTable(data, from, qtd) {
+            if(data[0] != null) return;
+            const firstKey = Object.keys(data).find(key => Array.isArray(data[key]) && data[key].length > 0);
+            if (firstKey) {
+                this.tableItems = data[firstKey].map(item => {
+                    return {
+                        delete: this.extractIdFromDeleteForm(item.Delete), 
+                        item_name: item["Item Name"], // Extrai o texto do nome do item
+                        price: item.Price,
+                        quantity: item.Quantity
+                    };
+                });
+            }
+        },
+        extractIdFromDeleteForm(htmlString) {
+            const parser = new DOMParser();
+            const doc = parser.parseFromString(htmlString, 'text/html');
+            const input = doc.querySelector("input[type='hidden'][name='ID']");
+            return input ? input.value : null;
+        },
+        getCookie(szName) {
+            var szValue = null;
+            if (document.cookie) {
+                var arr = document.cookie.split((escape(szName) + '='));
+                if (2 <= arr.length) {
+                    var arr2 = arr[1].split(';');
+                    szValue = unescape(arr2[0]);
+                }
+            }
+            return szValue;
+        },
+        translate(key) {
+            return window.langIndex[key] || key;
+        },
+        listshop() {
+            this.resultado = null;
+            this.showListAdmShop = true;
+            this.showAddItemShop = false;
+            this.showInfoPlayer = false;
+            this.get("/adm/shoplist?&DisplayStart=" + this.to + "&DisplayLength=" + this.from + "&sessionid=" + this.getCookie("sessionid"));
+        },
+        admshop() {
+            this.resultado = null;
+            this.showAddItemShop = true;
+            this.showListAdmShop = false;
+            this.showInfoPlayer = false;
+            
+        },
+        infoplayer() {
+            this.resultado = null;
+            this.showInfoPlayer = true;
+            this.showListAdmShop = false;
+            this.showAddItemShop = false;
+            
+        },
+        async deleteItem(itemId) { 
+            const response = await fetch(window.qualifyURL("/adm/deleteshop?ID=" + itemId + "&sessionid=" + this.getCookie("sessionid")));
+            const data = await response.text();
+            this.resultado = data;
+        },
+        async adm() {
+            console.log("Fetching player info:", this.nick, "Info type:", this.information);
+            const response = await fetch(window.qualifyURL("/adm/search?nick=" + this.nick + "&information=" + this.information + "&sessionid=" + this.getCookie("sessionid")));
+            const data = await response.json();
+            this.userInfo = data.map(user => ({
+                banned: user["Banned ?"],
+                canSell: user["Can Sell ?"],
+                isAdmin: user["is Admin ?"],
+                ip: user["Ip"],
+                canBuy: user["Can Buy ?"],
+                name: user["Name"],
+                websiteBanForm: user["WebSite Ban"]
+            }));
+        },
+        async additem() {
+            console.log("Adding item to shop:", this.itemId, "Quantity:", this.quantity, "Price:", this.price);
+            const response = await fetch(window.qualifyURL("/adm/addshop?&itemId=" + this.itemId + "&quantity=" + this.quantity + "&price=" + this.price + "&sessionid=" + this.getCookie("sessionid")));
+            const data = await response.text();
+            this.resultado = data;
+            this.resetAddItemForm();
+        },
+        resetAddItemForm() {
+            this.itemId = '';
+            this.quantity = '';
+            this.price = '';
+        },
+        async websiteban(formEvent) {
+            try {
+                let formDataObj = {};
+                for (let element of event.target.elements) {
+                    if (element.name && element.value) {
+                        formDataObj[element.name] = element.value;
+                    }
+                }
+                formDataObj['sessionid'] = this.getCookie("sessionid");
+                const params = new URLSearchParams(formDataObj);
+                const url = window.qualifyURL("/adm/webban") + "?" + params.toString();
 
-    return false;
-}
+                try {
+                    const response = await fetch(url);
 
-function admshop() {
-    $(".table").show();
-    $(".GetInfoPlayer").hide();
-    $(".listadmshop").hide();
-    $(".addItemShop").show();
-    return false;
-}
+                    if (!response.ok) {
+                        throw new Error(`HTTP error! status: ${response.status}`);
+                    }
 
-function hideall() {
-    $(".GetInfoPlayer").hide();
-    $(".addItemShop").hide();
-}
+                    const data = await response.text();
+                    this.resultado = data; // Supondo que 'resultado' seja uma propriedade reativa para exibir a resposta
+                } catch (error) {
+                    this.resultado = 'Error: ' + error.message;
+                }
 
-function infoplayer() {
-    $(".table").show();
-    $(".addItemShop").hide();
-    $(".listadmshop").hide();
-    $(".GetInfoPlayer").show();
-    return false;
-}
+            } catch (error) {
+                this.resultado = 'Error: ' + error.message;
+            }
+        },
+        async websiteunban(formEvent) {
+            try {
+                let formDataObj = {};
+                for (let element of event.target.elements) {
+                    if (element.name && element.value) {
+                        formDataObj[element.name] = element.value;
+                    }
+                }
+                formDataObj['sessionid'] = this.getCookie("sessionid");
+                const params = new URLSearchParams(formDataObj);
+                const url = window.qualifyURL("/adm/webunban") + "?" + params.toString();
 
-function replaceAll(find, replace, str) {
-    var regex = new RegExp(find, "igm");
-    return str.replace( regex , replace);
-}
+                try {
+                    const response = await fetch(url);
 
+                    if (!response.ok) {
+                        throw new Error(`HTTP error! status: ${response.status}`);
+                    }
+
+                    const data = await response.text();
+                    this.resultado = data; // Supondo que 'resultado' seja uma propriedade reativa para exibir a resposta
+                } catch (error) {
+                    this.resultado = 'Error: ' + error.message;
+                }
+
+            } catch (error) {
+                this.resultado = 'Error: ' + error.message;
+            }
+        },
+
+    },
+    mounted() {
+    // Aqui você pode inicializar componentes da página ou buscar dados iniciais
+    // Por exemplo, buscar a lista de itens do shop para exibir no "List Admin Shop"
+    // this.fetchShopItems();
+    }
+});
+
+window.vueApp = vue;
 function websiteban(form) {
-    hideall();
-    $.ajax({
-        url: window.qualifyURL("/adm/webban"),
-        data: $(form).serialize() + "&sessionid=" + getCookie("sessionid"),
-        success: function (data) {
-            $('#resultado').html(data);
-        },
-        error: function (error, data) {
-            $('#resultado').html(error);
-        },
-        dataType: "text"
-    });
+    window.vueApp.websiteban(form);   
     return false;
 }
 
 function websiteunban(form) {
-    hideall();
-    $.ajax({
-        url: window.qualifyURL("/adm/webunban"),
-        data: $(form).serialize() + "&sessionid=" + getCookie("sessionid"),
-        success: function (data) {
-            $('#resultado').html(data);
-        },
-        error: function (error, data) {
-            $('#resultado').html(error);
-        },
-        dataType: "text"
-    });
+    window.vueApp.websiteunban(form);
     return false;
 }
-
-
-function adm(form) {
-    $(".table").show();
-    $(".pagination").html("");
-    $.ajax({
-        url: window.qualifyURL("/adm/search"),
-        data: $(form).serialize() + "&sessionid=" + getCookie("sessionid"),
-        success: function (data) {
-            try {
-                var result = "";
-                $(".theadInput").html("");
-                $.each(data, function (key, val) {
-                    $.each(val, function (key, val) {
-                        $(".theadInput").html($(".theadInput").html() + "<th>" + key + "</th>");
-                    });
-                    return false;
-                });
-         
-                $.each(data, function (key, val) {
-                    result += "<tr>";
-                    $.each(val, function (key, val) {
-                        result += "<td>" + val + "</td>";
-                    });
-                    result += "</tr>";
-                });
-                
-                $(".tbodyInput").html(result);
-            } catch (err) {
-                $('#resultado').html(err);
-            };
-        },
-        error: function (error,msg) {
-            $('#resultado').html(error);
-        },
-        dataType: "json"
-    });
-    $(".GetInfoPlayer").hide();
-    return false;
-};
-
-function additem(form) {
-    $(".table").show();
-    $.ajax({
-        url: window.qualifyURL("/adm/addshop"),
-        data: $(form).serialize() + "&sessionid=" + getCookie("sessionid"),
-        success: function (data) {
-            if (data.indexOf("ok") == -1) {
-                $('#resultado').html(data);
-            } else {
-                $('#resultado').html("Sucess Create Server Auction");
-            }
-        },
-        error: function (error) {
-            $('#resultado').html(error);
-        },
-        dataType: "text"
-    });
-    $(".addItemShop").hide();
-    return false;
-};
-
-function del(form) {
-    $(form).hide();
-    $.ajax({
-        url: window.qualifyURL("/adm/deleteshop"),
-        data: $(form).serialize() + "&sessionid=" + getCookie("sessionid"),
-        success: function (data) {
-            $('#resultado').html(data);
-        },
-        error: function (error) {
-            $('#resultado').html(error);
-        },
-        dataType: "text"
-    });
-    $("#resultado").show();
-    return false;
-};
-
-function listshop(form) {
-    $("#resultado").hide();
-    $(".table").show();
-    hideall();
-    list(0, 10);
-    return false;
-};
-
-var to = 10;
-
-function list(to, from) {
-    $(".pagination").html("");
-    $.ajax({
-        url: window.qualifyURL("/adm/shoplist"),
-        data: "DisplayStart=" + to + "&DisplayLength=" + from + "&sessionid=" + getCookie("sessionid"),
-        success: function (data) {
-            try {
-                LoadTable(data);
-            } catch (err) {
-                $('#resultado').html(err);
-            };
-        },
-        error: function (error) {
-            $('#resultado').html(error);
-        },
-        dataType: "json"
-    });
-}
-
-var LoadTable = function (data) {
-    var result = "";
-    var total;
-    $(".theadInput").html("");
-    $.each(data, function (key, val) {
-        total = key;
-        $.each(val, function (key, val) {
-            $.each(val, function (key, val) {
-                $(".theadInput").html($(".theadInput").html() + "<th>" + key + "</th>");
-            });
-            return false;
-        });
-    });
-
-    $.each(data, function (key, val) {
-
-        $.each(val, function (key, val) {
-            result += "<tr>";
-            $.each(val, function (key, val) {
-                result += "<td>" + val + "</td>";
-            });
-            result += "</tr>";
-        });
-
-    });
-    
-        
-
-    $(".pagination").html("<a id='prev' href='#'>< Previous</a> Showing " + (to - 9) + " - " + to + " of " + total + " <a id='next' href='#'>Next ></a>");
-
-    if (total <= 10) {
-        $("#next").hide();
-        $("#prev").hide();
-    }
-        
-    
-    $("#next").click(function (e) {
-        e.preventDefault();
-        if (total > to) {
-            list(to, to + 10);
-            to = to + 10;
-        }
-    });
-    $("#prev").click(function (e) {
-        e.preventDefault();
-        if (to > 10) {
-            to = to - 10;
-            list(to - 10, to);
-        }
-    });
-    $(".tbodyInput").html(result);
-};
